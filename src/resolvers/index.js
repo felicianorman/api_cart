45ecbcc2ff25a686879d49861e384e82e172e3c7
Felicia <87656480+felicianorman@users.noreply.github.com>
2023-01-30 15:18:29 +0100

    skapade create cart funktion

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -6,6 +6,7 @@ const crypto = require("node:crypto");
 
 exports.resolvers = {
   Query: {
+    //Hämtar varukorg med ID
     getCartById: async (_, args) => {
       const cartId = args.cartId;
 
@@ -22,6 +23,35 @@ exports.resolvers = {
       });
       const data = JSON.parse(cartData);
       return data;
+    },
+  },
+  Mutation: {
+    createCart: async (_, args) => {
+      const newCart = {
+        id: crypto.randomUUID(),
+        totalAmount: args.totalAmount,
+      };
+
+      let filePath = path.join(__dirname, `../data/projects/${newCart.id}.json`);
+
+      let idExists = true;
+
+      while(idExists) {
+        const exists = await fileExists(filePath)
+        console.log(exists, newCart.id)
+        idExists = exists
+
+        if(exists) {
+          newCart.id = crypto.randomUUID()
+          filePath = path.join(__dirname, `../data/projects/${newCart.id}.json`)
+        }
+
+        idExists = exists
+
+        await fsPromises.writeFile(filePath, JSON.stringify(newCart));
+
+        return newCart
+      }
     },
   },
 };
